c374b3e3d184397e2982e0ede4144156a0749d80
Titouan Renaud <titouanrenaud@h069.visiteur.cnrs-gif.fr>
2026-01-08 10:36:10 +0100

    corrections

--- SRC/plot_TB(W).py
+++ SRC/plot_TB(W).py
@@ -1,3 +1,4 @@
+from matplotlib import testing
 import numpy as np
 import matplotlib.pyplot as plt
 from SRC.extract_data import extract_data, create_reg_array1, create_reg_array3, create_train_test_matrix
@@ -8,15 +9,11 @@ import matplotlib as mpl
 frame = extract_data()
 filter = create_convection_filter()
 
+train_mat, test_mat = create_train_test_matrix(prop_train=0.6)
 
-TB_filtered1830 = create_reg_array3('1830')
-TB_filtered1837 = create_reg_array3('1837')
-TB_filtered183T = create_reg_array3('183T')
-TB_filtered3250 = create_reg_array3('3250')
-TB_filtered3257 = create_reg_array3('3257')
-TB_filtered325T = create_reg_array3('325T')
-
-plt.figure()
-plt.imshow(testing, origin='lower', cmap='gray')
-plt.colorbar()
-plt.show()
+TB_filtered1830 = create_reg_array3('1830', frame, filter, train_mat)
+TB_filtered1837 = create_reg_array3('1837', frame, filter, train_mat)
+TB_filtered183T = create_reg_array3('183T', frame, filter, train_mat)
+TB_filtered3250 = create_reg_array3('3250', frame, filter, train_mat)
+TB_filtered3257 = create_reg_array3('3257', frame, filter, train_mat)
+TB_filtered325T = create_reg_array3('325T', frame, filter, train_mat)
